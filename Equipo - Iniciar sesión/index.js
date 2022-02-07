d2350e6dc15aacc198a2900eff8f78e014ce81df
Rafael García <98476957+RafaGarcia98@users.noreply.github.com>
2022-02-07 13:50:45 -0600

    Add files via upload

---
+++ Equipo - Iniciar sesión/index.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+router.get('/', function (req, res, next) {
+	return res.render('index.ejs');
+});
+
+
+router.post('/', function(req, res, next) {
+	console.log(req.body);
+	var personInfo = req.body;
+
+
+	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({email:personInfo.email},function(err,data){
+				if(!data){
+					var c;
+					User.findOne({},function(err,data){
+
+						if (data) {
+							console.log("if");
+							c = data.unique_id + 1;
+						}else{
+							c=1;
+						}
+
+						var newPerson = new User({
+							unique_id:c,
+							email:personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+
+						newPerson.save(function(err, Person){
+							if(err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({_id: -1}).limit(1);
+					res.send({"Success":"Ya estas registrado"});
+				}else{
+					res.send({"Success":"Tu email ya existe en la BD."});
+				}
+
+			});
+		}else{
+			res.send({"Success":"password no coincide"});
+		}
+	}
+});
+
+router.get('/login', function (req, res, next) {
+	return res.render('login.ejs');
+});
+
+router.post('/login', function (req, res, next) {
+	//console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+		if(data){
+			
+			if(data.password==req.body.password){
+				//console.log("Done Login");
+				req.session.userId = data.unique_id;
+				//console.log(req.session.userId);
+				res.send({"Success":"Correcto!"});
+				
+			}else{
+				res.send({"Success":" Falta password!"});
+			}
+		}else{
+			res.send({"Success":"este Email no esta registrado"});
+		}
+	});
+});
+
+router.get('/profile', function (req, res, next) {
+	console.log("profile");
+	User.findOne({unique_id:req.session.userId},function(err,data){
+		console.log("data");
+		console.log(data);
+		if(!data){
+			res.redirect('/');
+		}else{
+			//console.log("found");
+			return res.render('data.ejs', {"name":data.username,"email":data.email});
+		}
+	});
+});
+
+router.get('/logout', function (req, res, next) {
+	console.log("logout")
+	if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+    	if (err) {
+    		return next(err);
+    	} else {
+    		return res.redirect('/');
+    	}
+    });
+}
+});
+
+router.get('/forgetpass', function (req, res, next) {
+	res.render("forget.ejs");
+});
+
+router.post('/forgetpass', function (req, res, next) {
+	//console.log('req.body');
+	//console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+		console.log(data);
+		if(!data){
+			res.send({"Success":"Este email no esta registrado"});
+		}else{
+			// res.send({"Success":"Success!"});
+			if (req.body.password==req.body.passwordConf) {
+			data.password=req.body.password;
+			data.passwordConf=req.body.passwordConf;
+
+			data.save(function(err, Person){
+				if(err)
+					console.log(err);
+				else
+					console.log('Success');
+					res.send({"Success":"Password cambiada"});
+			});
+		}else{
+			res.send({"Success":"Ambas Password deben ser iguales"});
+		}
+		}
+	});
+	
+});
+
+module.exports = router;
